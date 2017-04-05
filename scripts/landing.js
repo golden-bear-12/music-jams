eae4872e54b032c4d8bbf9398a9b9a2f67677539
golden-bear-12 <m.honrychs@gmail.com>
2017-04-05 01:08:21 -0400

    added utilities.js + callbck

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,11 +1,16 @@
 var pointsArray = document.getElementsByClassName('point');
 
+var showPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";     
+}
+
 var animatePoints = function(points) {
+    forEach(points, showPoint)
     for(var i = 0; i < points.length; i++){
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)"; 
+        showPoint(i);
     }
 };
 
---
+++ scripts/utilities.js
@@ -0,0 +1,4 @@
+function forEach(parameter, callback) {
+    for(var i = 0; i < parameter.length; i++)
+        callback(parameter[i]);
+}
