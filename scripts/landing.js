64c26d8554b58334e0b487dbb7adf5c171a97822
golden-bear-12 <m.honrychs@gmail.com>
2017-04-09 22:19:21 -0400

    dom scripted album view

--- scripts/landing.js
+++ scripts/landing.js
@@ -5,7 +5,7 @@ var animatePoints = function(points) {
         points[i].style.opacity = 1;
         points[i].style.transform = "scaleX(1) translateY(0)";
         points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)"; 
+        points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
 };
 
@@ -13,13 +13,13 @@ window.onload = function() {
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
-    
+
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
 
     window.addEventListener('scroll', function(event) {
          if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+             animatePoints(pointsArray);
          }
     });
-}
+};
