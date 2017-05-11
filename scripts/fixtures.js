2a94cc0f6331b5a0d2932bc82f383adce50513bf
golden-bear-12 <m.honrychs@gmail.com>
2017-05-11 00:13:33 -0400

    next/previous jQuery functionality

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21'},
+        { title: 'Magenta', duration: '2:15'}
+    ]
+};
+
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello, Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21'},
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'}
+    ]
+};
+
+var albumSevenLions = {
+    title: 'Worlds Apart EP',
+    artist: 'Seven lions',
+    label: 'Casablanca Records',
+    year: '2014',
+    albumArtUrl: 'assets/images/album_covers/19.png',
+    songs: [
+        { title: "Don't Leave", duration: '6:02' },
+        { title: 'Worlds Apart', duration: '6:16' },
+        { title: 'Nepenthe', duration: '5:16'},
+        { title: 'Strangers', duration: '3:24' },
+        { title: 'Keep It Close', duration: '5:12'}
+    ]
+};
